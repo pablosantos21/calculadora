7da2555f998943019c6665296ffe73878604e068
Pablo <pablosantosbay@gmail.com>
2021-03-08 10:14:21 +0100

    calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend01';
+  cadena:string="";
+  numeroX:number=0;
+  numeroY:number=0;
+  calculo:string="";
+  borrarN:boolean=false;
+
+  addNumber(num:number){
+    if(this.borrarN==true)this.cadena="";
+    this.cadena+=num;
+    this.numeroX=+this.cadena;
+    this.borrarN=false;
+  }
+
+  operacion(op:string){
+    this.calculo=op;
+    this.cadena="";
+    this.numeroY = this.numeroX;
+    this.numeroX=0;
+  }
+
+  borrar(){
+    this.cadena="";
+    this.calculo="";
+    this.numeroX=0;
+    this.numeroY=0;
+  }
+
+  calcular(){
+    switch(this.calculo){
+      case "sumar":{
+        this.numeroX= this.numeroY+this.numeroX;
+        break
+      }
+      case "restar":{
+        this.numeroX=this.numeroY-this.numeroX;
+        break
+      }
+      case "multiplicar":{
+        this.numeroX=this.numeroY*this.numeroX;
+        break
+      }
+      case "dividir":{
+        this.numeroX=this.numeroY/this.numeroX;
+        break
+      }
+    }
+    this.numeroY=0;
+    this.calculo="";
+    this.borrarN=true;
+    
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
